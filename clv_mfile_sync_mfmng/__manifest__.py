56662410de0a6176bfbe64bac5d66f41a19b1e37
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-06 13:41:51 -0300

    [ADD] clv_mfile_sync_mfmng (Copied from branch 14.0)

---
+++ clv_mfile_sync_mfmng/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Media File External Sync (for Media File Management Solution)',
+    'summary': 'Media File External Sync Module used in Media File Management Solution.',
+    'version': '14.0.5.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_mfile',
+        'clv_external_sync_mfmng',
+    ],
+    'data': [
+        'data/mfile_marker_sync.xml',
+        'data/mfile_category_sync.xml',
+        'data/mfile_format_sync.xml',
+        'data/mfile_1_sync.xml',
+        'data/mfile_2_sync.xml',
+        'data/mfile_3_sync.xml',
+        'data/mfile_4_sync.xml',
+        'data/mfile_5_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
